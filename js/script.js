d5fca6c3d96f681d201f5b0b7e1d325c3e5b4d80
RominaMalimban <romina.lyn@hotmail.it>
2022-12-02 16:49:22 +0100

    metodo per far comparire dropdown menu

--- js/script.js
+++ js/script.js
@@ -17,16 +17,19 @@ createApp({
                 visible: true,
                 messages: [
                     {
+                        dropDownMenu : false,
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
                         status: 'sent'
                     },
                     {
+                        dropDownMenu : false,
                         date: '10/01/2020 15:50:00',
                         message: 'Ricordati di stendere i panni',
                         status: 'sent'
                     },
                     {
+                        dropDownMenu : false,
                         date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
                         status: 'received'
@@ -39,16 +42,19 @@ createApp({
                 visible: true,
                 messages: [
                     {
+                        dropDownMenu : false,
                         date: '20/03/2020 16:30:00',
                         message: 'Ciao come stai?',
                         status: 'sent'
                     },
                     {
+                        dropDownMenu : false,
                         date: '20/03/2020 16:30:55',
                         message: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received'
                     },
                     {
+                        dropDownMenu : false,
                         date: '20/03/2020 16:35:00',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'sent'
@@ -61,16 +67,19 @@ createApp({
                 visible: true,
                 messages: [
                     {
+                        dropDownMenu : false,
                         date: '28/03/2020 10:10:40',
                         message: 'La Marianna va in campagna',
                         status: 'received'
                     },
                     {
+                        dropDownMenu : false,
                         date: '28/03/2020 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent'
                     },
                     {
+                        dropDownMenu : false,
                         date: '28/03/2020 16:15:22',
                         message: 'Ah scusa!',
                         status: 'received'
@@ -83,11 +92,13 @@ createApp({
                 visible: true,
                 messages: [
                     {
+                        dropDownMenu : false,
                         date: '10/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent'
                     },
                     {
+                        dropDownMenu : false,
                         date: '10/01/2020 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
                         status: 'received'
@@ -100,11 +111,13 @@ createApp({
                 visible: true,
                 messages: [
                     {
+                        dropDownMenu : false,
                         date: '10/01/2020 15:30:55',
                         message: 'Ricordati di chiamare la nonna',
                         status: 'sent'
                     },
                     {
+                        dropDownMenu : false,
                         date: '10/01/2020 15:50:00',
                         message: 'Va bene, stasera la sento',
                         status: 'received'
@@ -117,16 +130,19 @@ createApp({
                 visible: true,
                 messages: [
                     {
+                        dropDownMenu : false,
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao Claudia, hai novità?',
                         status: 'sent'
                     },
                     {
+                        dropDownMenu : false,
                         date: '10/01/2020 15:50:00',
                         message: 'Non ancora',
                         status: 'received'
                     },
                     {
+                        dropDownMenu : false,
                         date: '10/01/2020 15:51:00',
                         message: 'Nessuna nuova, buona nuova',
                         status: 'sent'
@@ -139,11 +155,13 @@ createApp({
                 visible: true,
                 messages: [
                     {
+                        dropDownMenu : false,
                         date: '10/01/2020 15:30:55',
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
                         status: 'sent'
                     },
                     {
+                        dropDownMenu : false,
                         date: '10/01/2020 15:50:00',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
                         status: 'received'
@@ -156,16 +174,19 @@ createApp({
                 visible: true,
                 messages: [
                     {
+                        dropDownMenu : false,
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
                         status: 'received'
                     },
                     {
+                        dropDownMenu : false,
                         date: '10/01/2020 15:50:00',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                         status: 'sent'
                     },
                     {
+                        dropDownMenu : false,
                         date: '10/01/2020 15:51:00',
                         message: 'OK!!',
                         status: 'received'
@@ -223,6 +244,19 @@ createApp({
             };
         },
 
+        // Metodo per far comparire il dropdown menu sui singoli messaggi:
+        menuMessageClick(info){
+            if(info.dropDownMenu === true){
+                info.dropDownMenu = false;
+            }else{
+                info.dropDownMenu = true;
+            };
+        },
+
+        // Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+        deleteMessage(index){
+            this.contacts[this.activeChat].messages.splice(index, 1);
+        }
     }
     
   }).mount('#app');
